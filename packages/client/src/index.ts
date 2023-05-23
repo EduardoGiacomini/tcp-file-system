b233e26bbbe6ec14282c36afbea77350514362e2
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2023-05-22 21:51:05 -0400

    feat: create basic TCP client/server

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello world from client!");
+const net = require("node:net");
+
+const host = "127.0.0.1";
+const port = 3000;
+
+const client = net.createConnection(port, host, () => {
+  console.log("Connected");
+  client.write("add,1,1");
+});
+
+client.on("data", (data: string) => {
+  console.log(`Received: ${data}`);
+});
+
+client.on("error", (error: Error) => {
+  console.log(`Error: ${error.message}`);
+});
+
+client.on("close", () => {
+  console.log("Connection closed");
+});
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,45 @@
-console.log("Hello world from server!");
+import net from "node:net";
+
+const port = 3000;
+
+const server = net.createServer((socket) => {
+  console.log("Client connected");
+
+  socket.on("data", (data) => {
+    const strData = data.toString();
+    console.log(`Received: ${strData}`);
+
+    const command = strData.split(",");
+    const operator = command[0];
+    const operand1 = parseFloat(command[1]);
+    const operand2 = parseFloat(command[2]);
+    let result;
+
+    switch (operator) {
+      case "add":
+        result = operand1 + operand2;
+        break;
+      case "sub":
+        result = operand1 - operand2;
+        break;
+    }
+
+    socket.write(result?.toString() ?? "");
+  });
+
+  socket.on("end", () => {
+    console.log("Client disconnected");
+  });
+
+  socket.on("error", (error) => {
+    console.log(`Socket Error: ${error.message}`);
+  });
+});
+
+server.on("error", (error) => {
+  console.log(`Server Error: ${error.message}`);
+});
+
+server.listen(port, () => {
+  console.log(`TCP socket server is running on port: ${port}`);
+});
