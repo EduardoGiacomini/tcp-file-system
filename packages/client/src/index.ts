1e860f3e802fc9b555b36ea804391be681cc36ad
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2023-06-05 23:01:28 -0400

    feat: uses json as request and response

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -5,7 +5,7 @@ const port = 3000;
 
 const client = net.createConnection(port, host, async () => {
   console.log("Connected");
-  client.write("ls,");
+  client.write('{"command":"ls","argument":""}');
 });
 
 client.on("data", (data: string) => {
